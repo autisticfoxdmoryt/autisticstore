5929bf18e580b1231cc52fdbc8db4e46b0bf4c37
autisticfoxdmoryt <150083346+autisticfoxdmoryt@users.noreply.github.com>
2024-08-19 22:46:47 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('dark-mode').addEventListener('change', function() {
-    if (this.checked) {
-        document.body.style.backgroundColor = '#121212';
-        document.body.style.color = '#e0e0e0';
-    } else {
-        document.body.style.backgroundColor = '#1e1e1e';
-        document.body.style.color = '#ddd';
-    }
-});
